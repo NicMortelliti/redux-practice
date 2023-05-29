5253865375234fa725cfdb258992ce22edcaf25c
Nic Mortelliti <npmortelliti@gmail.com>
2023-05-29 13:05:47 -0700

    add new empty Checklist component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import Checklist from './Checklist';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Checklist />
+    </>
+  );
 };
 
 export default App;
