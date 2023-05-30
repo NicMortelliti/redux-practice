c9dd637324fc6d9e4dff0f8beda6f7b80d19effb
Nic Mortelliti <npmortelliti@gmail.com>
2023-05-30 12:05:08 -0700

    fetch json data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,11 @@
 import Checklist from './Checklist';
+import { useFetchData } from '../hooks/fetchers';
+
+const url = './ecl_data.json';
 
 const App = () => {
+  useFetchData(url);
+
   return (
     <>
       <Checklist />
