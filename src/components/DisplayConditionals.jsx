a2632fa61f307a9f0233038b73304d9d0901417f
Nic Mortelliti <npmortelliti@gmail.com>
2023-06-01 07:33:17 -0700

    add logic to set non-selected radios to false

--- src/components/DisplayConditionals.jsx
+++ src/components/DisplayConditionals.jsx
@@ -1,19 +1,38 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import useGetChildren from '../hooks/useGetChildren';
 import { toggleChecklistItemStatus } from '../redux/eclSlice';
 
 const DisplayConditionals = ({ obj }) => {
   const dispatch = useDispatch();
   const conditionalChildren = useGetChildren(obj);
+  const { children_ids } = obj;
+  const { data } = useSelector((state) => state.ecl);
 
   const getIdOfNonSelectedConditional = (selectedId) => {
     // We need to ensure only one radio button is selected.
     // So maybe we grab the id of the conditional item that is
     // not selected and make sure it's deselected (false)
+    const notSelectedIds = obj.children_ids.filter(
+      (eachChild) => eachChild !== selectedId
+    );
+
+    return notSelectedIds;
   };
 
   const handleToggle = (selectedId) => {
     dispatch(toggleChecklistItemStatus(selectedId));
+
+    // Set other radio buttons to false
+    const notSelectedIds = getIdOfNonSelectedConditional(selectedId);
+    notSelectedIds.map((eachNotSelectedId) => {
+      // Find only the id's of the other radio buttons that are set to true
+      if (
+        data.find((eachObj) => eachObj.id === eachNotSelectedId).completed ===
+        true
+      ) {
+        dispatch(toggleChecklistItemStatus(eachNotSelectedId));
+      }
+    });
   };
 
   return conditionalChildren.map((eachConditional) => {
@@ -21,10 +40,7 @@ const DisplayConditionals = ({ obj }) => {
     const active = completed ? 'active' : '';
 
     return (
-      <button
-        key={id}
-        className={active}
-        onClick={() => dispatch(toggleChecklistItemStatus(id))}>
+      <button key={id} className={active} onClick={() => handleToggle(id)}>
         {text}
       </button>
     );
