48dc98b75836f310f9536444276f13d8edf72e2b
Nic Mortelliti <npmortelliti@gmail.com>
2023-06-02 16:03:40 -0700

    change render style for msli children

--- src/components/DisplayConditionalParent.jsx
+++ src/components/DisplayConditionalParent.jsx
@@ -2,13 +2,21 @@ import ItemDirector from '../helpers/ItemDirector';
 import { useSelector } from 'react-redux';
 
 const DisplayConditionalParent = ({ obj }) => {
-  const { id: parentId, text, children_ids } = obj;
+  const { id: parentId, text, children_ids, type } = obj;
   const { data } = useSelector((state) => state.ecl);
 
   const RenderChildren = () =>
-    children_ids.map((id) => (
-      <ItemDirector key={id} id={id} parentId={parentId} />
-    ));
+    children_ids.map((id) => {
+      if (type === 'conditional_parent') {
+        return <ItemDirector key={id} id={id} parentId={parentId} />;
+      } else if (type === 'msli_parent') {
+        return (
+          <div key={id}>
+            <ItemDirector id={id} parentId={parentId} />
+          </div>
+        );
+      }
+    });
 
   const RenderChildrenOfSelectedConditional = () =>
     children_ids.map((id) => {
